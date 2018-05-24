d85a2cefe2db7ef02014156dcaa4db0c93dcda58
Andy Tang <andytang43@gmail.com>
2018-05-24 16:01:45 -0700

    revised tutorial

--- js/states/tutorial.js
+++ js/states/tutorial.js
@@ -11,7 +11,7 @@ var tutorial = {
         this.slickUI.add(new SlickUI.Element.Text(0, 10, 'How To Play', 40)).centerHorizontally();
         this.slickUI.add(new SlickUI.Element.Text(30, 100, 'Objective', 24));
         this.slickUI.add(new SlickUI.Element.Text(45, 140, '* Answer 25 questions correctly within 2 minutes, the game ends when 25 questions are correct'));
-        this.slickUI.add(new SlickUI.Element.Text(45, 165, '* Try to get as many points as possible, every question correct will give you 250 points, for every \n  question wrong, you lose 50 points'));
+        this.slickUI.add(new SlickUI.Element.Text(45, 165, '* Try to get as many points as possible, every question correct will give you 250 points, for every \n  question wrong, you lose 50 points, your score can also drop below 0'));
         this.slickUI.add(new SlickUI.Element.Text(30, 235, 'Controls', 24));
         if (mobileTester.isMobile()) {
             this.slickUI.add(new SlickUI.Element.Text(45, 275, "* Use the joystick to move"));
